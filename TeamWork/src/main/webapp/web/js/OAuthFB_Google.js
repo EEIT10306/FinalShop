1c6698934c4d8ae912c08e6601569805c5d3f322
林家禾 <gmgyade@gmail.com>
2018-11-01 18:40:00 +0800

    commit for pull 2.1.3 新增google登入

---
+++ TeamWork/src/main/webapp/web/js/OAuthFB_Google.js
@@ -0,0 +1,127 @@
+//========================FB登入==============================
+//初始化
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '333388330578468',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.2'
+    });
+
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+//引入 facebook SDK
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id))
+        return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/zh_TW/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+        console.log('Logged in and authenticated');
+        //           setElements(true);
+        testAPI();
+    } else {
+        console.log('Not authenticated');
+        //           setElements(false);
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+function testAPI() {
+    FB.api('/me?fields=name,first_name,last_name,email', function (response) {
+        if (response && !response.error) {
+            console.log(response);
+            $.ajax({
+                type:"post",
+                url: "/TeamWork/FBLogin",
+                data: {
+                    userInfo: JSON.stringify(response)
+                },
+                dataType: "json",
+                async:false,
+                // success: function (data) {
+                    // document.getElementById('status').innerHTML =
+                    //     'Thanks for logging in, ' + response.name + '!';
+                // }
+            });
+            //buildProfile(response);
+        }
+        //           FB.api('/me/feed', function(response){
+        //             if(response && !response.error){
+        //               buildFeed(response);
+        //             }
+        //           });
+    })
+}
+
+// =================google登入==========================
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    alert("google登出")
+}
+
+// 		function onSignIn(googleUser) {
+// 			//跳?到http://gntina.iok.la/sendRedirect(?取用?信息)
+// 			alert(profile.getEmail())
+//              location.href = "www.google.com";
+// 			//?取用?基本信息，但是此id不能?后台用，不安全，改用id_token
+// 			/*??往下的代?都不需要，因?是在后台??，后台?取用?信息  */
+//             var profile = googleUser.getBasicProfile();
+// 			console.log('google自己封?好的?取用?信息');
+// 			console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+// 			console.log('Name: ' + profile.getName());
+// 			console.log('Image URL: ' + profile.getImageUrl());
+// 			console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+// 			//?id_token?送?后台?行??
+// 			/*var id_token = googleUser.getAuthResponse().id_token;
+// 			var xhr = new XMLHttpRequest();
+// 			xhr.open('POST', 'http://gntina.iok.la/idToken');
+// 			xhr.setRequestHeader('Content-Type',
+// 					'application/x-www-form-urlencoded');
+// 			xhr.onload = function() {
+// 				console.log('Signed in as: ' + xhr.responseText);
+// 			};
+// 			xhr.send('idtoken=' + id_token);
+//                        */
+// ?               }
+function onSignIn(googleUser) {
+
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    //測試有無進入google登入的方訊
+    alert("這裡是google的登入:" + profile.getEmail())
+
+    $.ajax({
+        type: "POST",
+        url: "processGoogleLogin",
+        data: {
+            name: profile.getName(),
+            email: profile.getEmail()
+        },
+        success: function (data) {
+            alert('這裡是google的登入成功後的方訊 :' + data);
+            window.location.href = "http://localhost:8090/FinalProject/git/repository/FinalProject/src/main/webapp/FirstPage.html";
+        }
+    });
+}
