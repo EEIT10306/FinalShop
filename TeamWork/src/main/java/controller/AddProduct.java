9ceb8bba1a7e21a665c1b7036d45409548148142
林家禾 <gmgyade@gmail.com>
2018-11-09 14:07:16 +0800

    header 新增選項 許願 開團 開店 基礎新增完成 後續增加判斷內容

--- TeamWork/src/main/java/controller/AddProduct.java
+++ TeamWork/src/main/java/controller/AddProduct.java
@@ -8,19 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+
+import model.bean.GroupProduct;
 import model.bean.StoreProduct;
+import model.bean.WishProduct;
+import model.service.GroupProductService;
 import model.service.StoreProductService;
+import model.service.WishProductService;
 
 @Controller
 public class AddProduct {
 	
 	@Autowired
 	StoreProductService storeProductService;
+	@Autowired
+	GroupProductService groupProductService;
+	@Autowired
+	WishProductService wishProductService;
 	
 	@RequestMapping(value = "/AddStoreProduct" , method = RequestMethod.POST)
 	@ResponseBody
 	private String AddStoreProduct(StoreProduct storeProduct) {
-		
+		Gson gson = new Gson();
 		System.out.println(storeProduct.toString());
 		System.out.println("AddStoreProduct");
 		storeProduct.setS_id(1);
@@ -30,9 +40,37 @@ public class AddProduct {
 		storeProduct.setsP_averageAssess(2.0);
 		storeProduct.setsP_amountAssess(1);
 		storeProduct.setsP_stateId(13);
-		storeProductService.insert(storeProduct);
-		System.out.println(storeProduct);
-		return storeProduct.getsP_transport();
+		StoreProduct list = storeProductService.insert(storeProduct);
+		String json = gson.toJson(list);
+		
+		System.out.println("json = " + json);
+		return json;
+		
+	}
+	
+	@RequestMapping(value = "/AddGroupon" , method = RequestMethod.POST)
+	@ResponseBody
+	private String AddGroupon(GroupProduct groupProduct) {
+		Gson gson = new Gson();
+		groupProduct.setG_iD(5);
+		groupProduct.setgP_stateId(53);
+		GroupProduct list = groupProductService.insert(groupProduct);
+		System.out.println(list);
+		return null;
+		
+		
+	}
+	
+	@RequestMapping(value = "/AddWish" , method = RequestMethod.POST)
+	@ResponseBody
+	private String AddWish(WishProduct wishProduct) {
+		Gson gson = new Gson();
+		wishProduct.setW_id(5);
+		wishProduct.setwP_stateId(32);
+		WishProduct list = wishProductService.insert(wishProduct);
+		System.out.println(list);
+		return null;
+		
 		
 	}
 }
