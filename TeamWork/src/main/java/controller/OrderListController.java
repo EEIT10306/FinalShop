5e4d018b00dc364bd950e8fb9243fb3f83a6b6fd
曾敏婷 <mitti@mitti.com>
2018-11-13 19:37:55 +0800

    請家禾大大幫我加header吧!!!!!!

--- TeamWork/src/main/java/controller/OrderListController.java
+++ TeamWork/src/main/java/controller/OrderListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomNumberEditor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -12,10 +13,10 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import model.bean.GroupAssess;
 import model.bean.GroupOrder;
-import model.bean.Member;
 import model.bean.StoreAssess;
 import model.bean.StoreOrder;
 import model.bean.Wish;
@@ -23,16 +24,30 @@ import model.bean.WishAssess;
 import model.bean.WishBid;
 import model.bean.WishOrder;
 import model.service.OrderService;
+import model.service.UserPageImageService;
 
 @Controller
 public class OrderListController {
 
 	@Autowired
 	OrderService orderService;
+	@Autowired
+	UserPageImageService userPageImageService;
 
 	@InitBinder
 	protected void InitBinder(WebDataBinder binder) {
 		binder.registerCustomEditor(Integer.class, new CustomNumberEditor(Integer.class, false));
+	}
+	
+	// 更新使用者圖像
+	@RequestMapping(value = "/UpdateAccountImage", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
+	public @ResponseBody String updateAccountImage(List<MultipartFile> files,Integer m_id) {
+		System.out.println(files);
+		//執行上傳圖片方法
+		for(MultipartFile file : files) {			
+			userPageImageService.insertAccountImage(file, m_id);
+		}
+		return "updateAccountImage Success!";
 	}
 
 	// 取得商店訂單資料
