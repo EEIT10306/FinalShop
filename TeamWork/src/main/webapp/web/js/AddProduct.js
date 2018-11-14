3891348621fe864b44fe7d447a78ab97d12f524e
林家禾 <gmgyade@gmail.com>
2018-11-14 10:27:40 +0800

    火狐和chrome target測試

--- TeamWork/src/main/webapp/web/js/AddProduct.js
+++ TeamWork/src/main/webapp/web/js/AddProduct.js
@@ -1,6 +1,8 @@
 //===================圖片瀏覽======================
 function imgPreview(fileDom) {
-    var id = $(event.target).next().next().attr("id");
+    var theEvent = arguments.callee.caller.arguments[0];
+    var target = theEvent.target | event.target;
+    var id = $(target).next().next().attr("id");
     // alert("img======="+document.getElementById("Storepreview"))
     // alert("img2======="+img)
     //判斷是否支援filereader
