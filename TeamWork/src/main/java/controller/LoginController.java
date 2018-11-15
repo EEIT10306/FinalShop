52900df17b161f0ee316d12cbf2b7479a864d0ee
林家禾 <gmgyade@gmail.com>
2018-11-15 14:12:44 +0800

    cookies更改傳回bean

--- TeamWork/src/main/java/controller/LoginController.java
+++ TeamWork/src/main/java/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+
 import model.bean.Member;
 import model.service.MemberService;
 
@@ -165,13 +167,14 @@ public class LoginController {
 	
 	}
 	
-	@RequestMapping(value = "/checkAccount" , method = RequestMethod.POST ,produces="text/html;charset=utf-8")
+	@RequestMapping(value = "/checkAccount" , method = RequestMethod.POST ,produces = "application/json; charset=utf-8")
 	@ResponseBody
 	private String checkAccount(Member member) {
 		System.out.println("LoginCheckAccount");
 		//接收資料
 		//驗證資料
 		//呼叫model
+		Gson gson = new Gson();
 		System.out.println(member);
 		String hql = "WHERE m_account = '"+member.getM_account()+"'";
 		
@@ -186,20 +189,22 @@ public class LoginController {
 		if(bean.size()==0||bean.isEmpty()||bean==null) {
 			return "accountNoRepeat";
 		}
-		for(Member mbean:bean) {
-			System.out.println(mbean);
-			if("facebook".equals(mbean.getM_password()) || mbean.getM_password()=="facebook"){
-				System.out.println("FB帳號");
-				return mbean.getM_account().toString()+"facebook";
-			} else if("google".equals(mbean.getM_password()) || mbean.getM_password()=="google") {
-				System.out.println("google帳號");
-				return mbean.getM_account().toString()+"google";
-			} else {
-				System.out.println("一般帳號");
-				return mbean.getM_account().toString();
-			}
-			
-		}
-		return "account";
+		String json = gson.toJson(bean);
+		return json;
+//		for(Member mbean:bean) {
+//			System.out.println(mbean);
+//			if("facebook".equals(mbean.getM_password()) || mbean.getM_password()=="facebook"){
+//				System.out.println("FB帳號");
+//				return mbean.getM_account().toString()+"facebook";
+//			} else if("google".equals(mbean.getM_password()) || mbean.getM_password()=="google") {
+//				System.out.println("google帳號");
+//				return mbean.getM_account().toString()+"google";
+//			} else {
+//				System.out.println("一般帳號");
+//				return mbean.getM_account().toString();
+//			}
+//			
+//		}
+//		return "account";
 	}
 }
